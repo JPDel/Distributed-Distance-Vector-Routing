e2283e306b57da76e49645abdfc151528b27c09a
Edwin <evasquez4999@gmail.com>
2020-11-29 18:17:34 -0500

    2nd commit

--- network_init.py
+++ network_init.py
@@ -1,6 +1,22 @@
 import threading  # Used for multithreading
+import socket
+
+
+in_file = open("network.txt", "r")
 
 
 # Note: I'll probably try to implement with a single thread before attempting with multithreading
 def network_init():
-    return 0
+    return
+
+
+# This is intended only to count how many nodes provided by any given text file
+def count_nodes():
+    string = in_file.readline()
+    list = string.split()
+    in_file.seek(0, 0)  # Resets read head on the file back to the beginning
+    return len(list)
+
+
+if __name__ == '__main__':
+    network_init()
