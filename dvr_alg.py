439e027ac05d66b5d3853eb7961e46883b69f9f2
Edwin <evasquez4999@gmail.com>
2020-12-01 17:44:24 -0500

    Updated comments for clarity

--- dvr_alg.py
+++ dvr_alg.py
@@ -1,7 +1,6 @@
 def dvr_alg(r_node, adj_mat):
     init_find_neighbors(r_node, adj_mat)
-    mark_inf_vals(r_node)
-    print(r_node.dvr_matrix)
+    mark_inf_values(r_node)
 
     broadcast(r_node)
 
@@ -23,14 +22,14 @@ def init_find_neighbors(r_node, adj_mat):
 
 # Sets all values that should be initially infinity as such.
 # These are all set to an integer value of '999' as specified in the instructions
-def mark_inf_vals(r_node):
+def mark_inf_values(r_node):
     row_num = r_node.num - 1
 
     for i in range(0, 5):
 
         r_node.dvr_matrix[row_num][i] = 999
         for j in range(0, 5):
-            if(j != i) and (r_node.dvr_matrix[row_num][j] == 0):
+            if (j != i) and (r_node.dvr_matrix[row_num][j] == 0):
                 r_node.dvr_matrix[row_num][j] = 999
 
         r_node.dvr_matrix[i][row_num] = 999
@@ -38,11 +37,16 @@ def mark_inf_vals(r_node):
             if r_node.dvr_matrix[i][j] == 0:
                 r_node.dvr_matrix[i][j] = 999
 
-    #r_node.dvr_matrix[row_num][row_num] = 0 # Marks a node's distance to itself as 0
+    # Marks a node's distance to itself as 0
+    # r_node.dvr_matrix[row_num][row_num] = 0
 
     return
 
 
+# This function broadcasts any new shortest path estimates
+#
+# Return: - Upon broadcasting a new DVR estimate, this function will return True
+#         - If no shortest path was found for broadcasting, then False is returned
 def broadcast(r_node):
     # For testing: updated_rows = [None, [999, 2, 0, 0, 0], None, None, [999, 0, 0, 0, 1]]
     no_broadcast_required = [None, None, None, None, None]
@@ -51,13 +55,13 @@ def broadcast(r_node):
     # For testing: print("Broadcasting rows: " + str(updated_rows))
     # For testing: print("Broadcasting string: " + form_bc_string(updated_rows, r_node.num))
 
-    if(updated_rows != no_broadcast_required):
+    if updated_rows != no_broadcast_required:
         # broadcast the DVR estimates
         # TODO: TCP Socket stuff goes here
-        return
+        return True
     else:
         # stop execution
-        return
+        return False
 
 
 # Checks the node's DVR matrix for rows with any new shortest paths found during the last update
@@ -76,18 +80,21 @@ def select_bc_rows(r_node):
                     shorter_path_found = True
                     for k in range(0, 5):  # And compare that new value to the rest of the values in that row
 
-                        if (j != k) and (cur_dvr[i][k] != 0) and (cur_dvr[i][k] != 999) and (cur_dvr[i][j] >= cur_dvr[i][k]):
+                        if (j != k) and (cur_dvr[i][k] != 0) and (cur_dvr[i][k] != 999) and \
+                                (cur_dvr[i][j] >= cur_dvr[i][k]):
                             # If the new value does not provide a new shortest path
                             shorter_path_found = False  # Do not broadcast that row
 
                     if shorter_path_found:  # Adds that row to the array about to be broadcast
+                                            # if a shorter path is found
+
                         ret_rows[i] = cur_dvr[i]
     return ret_rows
 
 
 # Assembles the string of data that will be broadcast through TCP connection
 # This is meant to be called within broadcast()
-# Parameters: rows - An array of rows from the dvr that the node will broadcast
+# Parameters: rows - An array of rows from the dvr matrix that the node will broadcast
 #             node_num - The number of the node
 #
 # Return: Returns a string of format:
